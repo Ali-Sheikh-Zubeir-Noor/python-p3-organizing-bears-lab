39e7392cb45a7b193884bb202119d4ddcb7721ae
Ali Sheikh Zubeir Noor <ali.noor1@student.moringaschool.com>
2025-06-01 06:54:27 +0300

    Adds changes

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id TYPE INTEGER PRIMARY KEY,
+  name TYPE TEXT,
+  age TYPE INTEGER,
+  sex TYPE TEXT,
+  color TYPE TEXT,
+  temperament TYPE TEXT,
+  alive TYPE boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 12, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 10, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 7, 'F', 'Blonde', 'Sweet', 1),
+('Sergeant Brown', 11, 'M', 'Dark Brown', 'Bold', 1),
+('Melissa', 5, 'F', 'White', 'Gentle', 1),
+('Grinch', 9, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 6, 'F', 'Grey', 'Playful', 1),
+(NULL, 4, 'M', 'Golden', 'Curious', 1);
+
